d0bc55647b410798c87b78cc5f7f17933dc795bc
tux <tux@aktt2-woehrer.mshome.net>
2024-03-26 18:41:28 +0100

    incremented version and updated image build script

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo docker build -t tederer/windsensor-service .
+scriptDir=$(cd $(dirname $0) && pwd)
+cd $scriptDir
 
+imageVersion=$(jq --raw-output .version package.json)
+
+if [ "$imageVersion" == "" ]; then
+   echo "ERROR: failed to read version from package.json"
+   exit 1
+fi
+
+sudo docker build -t tederer/windsensor-service:$imageVersion .
